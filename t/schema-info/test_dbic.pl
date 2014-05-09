da91dc4126db1514a45b1788003d35b1e9d2834a
Stefan Hornburg (Racke) <racke@linuxia.de>
2014-05-09 10:39:40 +0200

    Add relationship tests on Company::Employee class.

--- t/schema-info/test_dbic.pl
+++ t/schema-info/test_dbic.pl
@@ -6,6 +6,7 @@ BEGIN {
 }
 
 use TableEdit::SchemaInfo;
+use TableEdit::SchemaInfo::Test::Relationships qw(test_relationships);
 
 isa_ok(Schema, 'DBIx::Class::Schema');
 
@@ -19,5 +20,26 @@ my $expected_value = 10;
 
 ok ($count == $expected_value, "Test number of classes")
         || diag "Number of classes: $count instead of $expected_value.";
+
+# Test relationships
+my %expected = (
+    job => {
+        type => 'belongs_to',
+        self_column => 'fk_job_id',
+        foreign_column => 'job_id',
+    },
+    company => {
+        type => 'belongs_to',
+        self_column => 'fk_company_id',
+        foreign_column => 'company_id',
+    },
+    employee => {
+        type => 'belongs_to',
+        self_column => 'fk_employee_id',
+        foreign_column => 'employee_id',
+    },
+);
+
+test_relationships($schema_info->classes->{'Company::Employee'}, \%expected);
 
 done_testing;
