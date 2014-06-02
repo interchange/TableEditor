2e2baef9d871d4a19ea1f105e2eb4efc432bc737
gregapompe <grega.pompe@informa.si>
2014-06-02 13:14:38 +0200

    Added some plugin documentation and a few fixes for plugins

--- lib/TableEdit/Plugins/Locator/API.pm
+++ lib/TableEdit/Plugins/Locator/API.pm
@@ -1,6 +1,11 @@
 package TableEdit::Plugins::Locator::API;
 use Dancer ':syntax';
 
+use Dancer::Plugin::Auth::Extensible;
+use Dancer::Plugin::DBIC qw(schema resultset rset);
+use TableEdit::SchemaInfo;
+
+my $schema_info;
 
 prefix '/api';
 
@@ -20,9 +25,20 @@ post '/Event' => sub {
 	pass;
 };
 
-post '/:class' => sub {
-	debug "Validating ". params->{class}."...";
-	pass;
+get '/Event' => require_login sub {
+	my (@languages, $errorMessage);
+	my $class = 'Event';
+	$schema_info ||= TableEdit::SchemaInfo->new(schema => schema);
+	my $columns = [map {$_->hashref} @{TableEdit::Routes::API::columns_info($class, TableEdit::Routes::API::class_form_columns($class))}];
+	my $relationships = [map {$_->hashref} $schema_info->relationships($class)];
+
+	return to_json({ 
+		custom_information => 'this is custom!',
+		fields => $columns,
+		class => $class,
+		class_label => $schema_info->{$class}->{label},
+		relations => $relationships,
+	}, {allow_unknown => 1}); 
 };
 
 1;
