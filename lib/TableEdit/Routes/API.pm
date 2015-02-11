94b93c3bb79fa34cf5f08d0733b9f728b3c6f36b
gregapompe <grega.pompe@informa.si>
2015-02-11 15:00:00 +0100

    Return PK string insted of list of PKs. Caused Odd number od items in hash.

--- lib/TableEdit/Routes/API.pm
+++ lib/TableEdit/Routes/API.pm
@@ -360,10 +360,11 @@ post '/:class' => require_login sub {
 	debug "Updating item for ".$class_info->name.": ", $item;
 	my $object = $class_info->resultset->update_or_create( $item->{values} );
 	return to_json {error => 'Unable to save.'} unless  $object;
+	my $rowInfo = schema_info->row($object);
 	my $object_hash = {
 		name => schema_info->row($object)->to_string,
 		values => {$object->get_columns},
-		id => $object->id,
+		id => $rowInfo->primary_key_string,
 		new_redirect => $class_info->attr('new_redirect'),
 	};
 	return to_json ($object_hash,{allow_unknown => 1});
