interchange/TableEditor

commit 57ba93021dd17a643dba9d3f69b6a6397c92de08

fix LOWER query in API get '/:class/autocomplete'

lib/TableEdit/Routes/API.pm
@@ -240,14 +240,21 @@ get '/:class/autocomplete' => require_login sub {
 	
 	my $search_columns = $class_info->search_columns;
 	
-	my $items = $class_info->resultset->search(
-		{
-			'-or' => {
-				map {"LOWER(".$_.")" => {'-like' => '%'.lc param('q').'%'} } @$search_columns
-			}
-		},
-		{rows => 10}
-	);
+    # lhs LIKE ?, [ bind_value ]
+    my $items = $class_info->resultset->search(
+        {
+            [
+                map {
+                    \[
+                        "LOWER($_) LIKE ?",
+                        [ { dbic_colname => $_ }, '%' . lc param('q') . '%' ]
+                     ]
+                } @$search_columns
+
+            ]
+        },
+        { rows => 10 }
+    );
 	
 	my @results;
 	for my $item ($items->all){

commit 6200b1c81e3482492e741856f35ce5a435e6181c

fix incorrect search condition introduced in 57ba93021dd1

lib/TableEdit/Routes/API.pm
@@ -240,19 +240,19 @@ get '/:class/autocomplete' => require_login sub {
 	
 	my $search_columns = $class_info->search_columns;
 	
+    print STDERR to_dumper $search_columns;
+
     # lhs LIKE ?, [ bind_value ]
     my $items = $class_info->resultset->search(
-        {
-            [
-                map {
-                    \[
-                        "LOWER($_) LIKE ?",
-                        [ { dbic_colname => $_ }, '%' . lc param('q') . '%' ]
-                     ]
-                } @$search_columns
+        [
+            map {
+                \[
+                    "LOWER($_) LIKE ?",
+                    [ { dbic_colname => $_ }, '%' . lc param('q') . '%' ]
+                  ]
+              } @$search_columns
 
-            ]
-        },
+        ],
         { rows => 10 }
     );
 	
@@ -494,6 +494,7 @@ sub grid_template_params {
 	$class_info->page_size($get_params->{page_size}) if $get_params->{page_size};
 	my $page_size = $class_info->page_size;
 	
+    print STDERR to_dumper $where;
 	my $rows = $rs->search(
 	$where,
 	  {
@@ -572,6 +573,7 @@ Table alias.
 
 sub grid_where {
 	my ($class_info, $where, $params, $alias) = @_;
+    print STDERR to_dumper $where;
 	$alias ||= 'me';
 	my @columns = $class_info->columns;
 	for my $column (@columns) {
@@ -587,6 +589,7 @@ sub grid_where {
 			
 			if ($column->{data_type} and ! ref $condition and ($column->{data_type} eq 'text' or $column->{data_type} eq 'varchar')){
 				delete $where->{$sql_name};
+                #$where->{"LOWER($sql_name)"} = {'-like' => "%".lc $condition."%"} if defined $condition and $condition ne '';
 				$where->{"LOWER($sql_name)"} = {'-like' => "%".lc $condition."%"} if defined $condition and $condition ne '';
 			}
 			else { 