ff33dc33d442be62b22a576f35d50c20a238aabf
Peter Mottram <peter@sysnix.com>
2015-05-19 07:32:49 -0400

    improved m2m introspection code for #36

--- lib/TableEdit/DBIC.pm
+++ lib/TableEdit/DBIC.pm
@@ -4,32 +4,28 @@ package    # hide from PAUSE
 use strict;
 use warnings;
 
+use DBIx::Class::Relationship::Helpers;
+use DBIx::Class::ResultSourceProxy::Table;
+use Class::Method::Modifiers qw/install_modifier/;
+
 # DBIx::Class::ResultSourceProxy::Table->table is almost certainly not the
 # best place to add class methods - to be investigated further.
 
-use DBIx::Class::ResultSourceProxy::Table;
-package    # hide from PAUSE
-  DBIx::Class::ResultSourceProxy::Table;
-use Class::Method::Modifiers;
-
-before "table" => sub {
+install_modifier "DBIx::Class::ResultSourceProxy::Table", "before",
+  "table", sub {
     my ( $class, $table ) = @_;
     return unless $table;
     $class->mk_classdata( _table_editor_m2m_metadata => {} )
       unless $class->can('_table_editor_m2m_metadata');
-};
+  };
 
 # add m2m metadata in DBIx::Class::Relationship::Helpers
 
-use DBIx::Class::Relationship::Helpers;
-package    # hide from PAUSE
-  DBIx::Class::Relationship::Helpers;
-use Class::Method::Modifiers;
-
-before "many_to_many" => sub {
+install_modifier "DBIx::Class::Relationship::Helpers", "before",
+  "many_to_many", sub {
     my ( $class, $meth, $rel, $f_rel ) = @_;
     $class->_table_editor_m2m_metadata->{$meth} =
       { local => $rel, foreign => $f_rel };
-};
+  };
 
 1;
